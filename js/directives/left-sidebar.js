4838dc1f6d36a272e5eb93f3c966055c435a95a3
evgenitochev <evgenitochev@gmail.com>
2015-01-06 15:37:47 +0200

    add left-sidebar

---
+++ js/directives/left-sidebar.js
@@ -0,0 +1,9 @@
+app.directive('leftPublicSidebar', function() {
+	return {
+		 controller: 'HomeCtrl',
+		 restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		 templateUrl: 'templates/public/left-sidebar.html',
+		 replace: true,
+			
+		}
+});
