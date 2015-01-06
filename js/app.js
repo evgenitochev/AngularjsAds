c253e7afb7b22493af1597108cafef7b7772daf0
evgenitochev <evgenitochev@gmail.com>
2015-01-06 17:55:28 +0200

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@ var app = angular.module('adsApp', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider){
 	$routeProvider.when('/', {
-		templateUrl: '/templates/home.html',
+		templateUrl: 'templates/home.html',
 		controller: 'HomeCtrl'
 	});
 	$routeProvider.when('/login', {
-		templatUrl: '/templates/login.html',
+		templateUrl: 'templates/login.html',
 		controller: 'LoginCtrl'
 	});
 	$routeProvider.when('/register', {
-		templatUrl: '/templates/register.html',
+		templateUrl: 'templates/register.html',
 		controller: 'RegisterCtrl'
 	});
 	$routeProvider.otherwise({
