05e054282d23c2d76a9f76bc3b193b5aa43b92bf
evgenitochev <evgenitochev@gmail.com>
2015-01-07 17:02:37 +0200

    access_token

--- js/services/data/userData.js
+++ js/services/data/userData.js
@@ -1,9 +1,13 @@
-app.factory('userData', ['$resource', 'baseServiceUrl', function($resource,baseServiceUrl){
+app.factory('userData', ['$resource', 'baseServiceUrl','authentication', function($resource,baseServiceUrl,authentication){
 	
 
 	function registerUser (user) {
 		return $resource(baseServiceUrl + 'user/register')
-			.save(user);
+			.save(user)
+			.$promise
+			.then(function (data) {
+				authentication.saveUser(user);
+			});
 	}
 
 	function loginUser (user) {
