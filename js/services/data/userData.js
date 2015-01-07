b5eec435f3eeb0d2abecc2b8ba7fdce38dd78726
evgenitochev <evgenitochev@gmail.com>
2015-01-07 16:41:31 +0200

    add userData, townsData

---
+++ js/services/data/userData.js
@@ -0,0 +1,24 @@
+app.factory('userData', ['$resource', 'baseServiceUrl', function($resource,baseServiceUrl){
+	
+
+	function registerUser (user) {
+		return $resource(baseServiceUrl + 'user/register')
+			.save(user);
+	}
+
+	function loginUser (user) {
+		// body...
+	}
+
+	function logoutUser (user) {
+		// body...
+	}
+
+
+
+	return {
+		register: registerUser,
+		login: loginUser,
+		logout: logoutUser
+	}
+}])
