66791ae0231c168634a4cd474f1d3aa0ec3e6996
evgenitochev <evgenitochev@gmail.com>
2015-01-10 23:57:52 +0200

    add filter.js

--- js/controllers/CategoriesCtrl.js
+++ js/controllers/CategoriesCtrl.js
@@ -1,9 +1,16 @@
-app.controller('CategoriesCtrl', ['$scope','categoriesData', function($scope,categoriesData){
-	categoriesData.getCategories()
+app.controller('CategoriesCtrl', ['$scope','categoriesData','filter', function($scope,categoriesData,filter){
+	
+
+
+		categoriesData.getCategories()
 		.$promise
 		.then(function (data) {
 			$scope.categories = data;
-		})
+		});
 
 
-}])
+
+	$scope.categoryClicked = function categoryClicked(category) {
+		filter.filterByCategory(category);
+	}
+}]);
